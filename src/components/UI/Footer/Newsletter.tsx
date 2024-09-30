e9a697def545c97d681466e933b573860a00ebc4
Soobarna <subarnapokharel252@gmail.com>
2024-09-30 17:24:48 +0545

    Footer

--- src/components/UI/Footer/Newsletter.tsx
+++ src/components/UI/Footer/Newsletter.tsx
@@ -1,5 +1,29 @@
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <section className="bg-yellow px-[120px] py-[80px] ">
+      <div className="flex items-center justify-between max-w-[1440px] mx-auto">
+        <h2 className="text-h2 font-bold leading-[120%] text-blue w-[700px] ">
+          Want more recipes straight to your inbox?
+        </h2>
+        <div className="">
+          <p className="text-pl leading-[160%] text-blue">
+            Sign up for the newsletter
+          </p>
+          <input
+            type="text"
+            placeholder="Enter your email"
+            className="w-full px-[16px] py-[12px] border-b-2 border-black"
+          />
+          <button
+            type="submit"
+            className="block border border-blue uppercase text-blue font-semibold text-pxs w-full mt-2 px-[16px] py-[8px] hover:text-green transition-colors duration-150 "
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Newsletter;
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,13 @@
+import Footers from "../UI/Footer/Footer";
+import Newsletter from "../UI/Footer/Newsletter";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <Newsletter />
+      <Footers />
+    </>
+  );
 };
 
 export default Footer;
